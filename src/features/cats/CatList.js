f8e6c8524c56371c22d40d9d8a56944092b5f777
zahraa7 <zahraa.knass@hotmail.com>
2022-04-07 01:52:45 +0300

    done

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
-// write your CatList component here
+import React from "react";
+
+export default function CatList({ catPics }) {
+  return (
+    <>
+      {catPics.map((image, i) => (
+        <img key={i} src={image.url} alt="cat" />
+      ))}
+    </>
+  );
+}
--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,40 @@
-export function fetchCats() {}
-
-export default function catsReducer() {}
+export function fetchCats() {
+    return (dispatch) => {
+      dispatch({
+        type: 'cats/catsLoading',
+      });
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          dispatch({
+            type: 'cats/catsLoaded',
+            payload: data.images, 
+          });
+        });
+    };
+  }
+  
+  const initialState = {
+    entities: [], 
+    status: 'idle', 
+  };
+  
+  export default function catsReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'cats/catsLoading':
+        return {
+          ...state,
+          status: 'loading',
+        };
+      case 'cats/catsLoaded':
+        return {
+          ...state,
+          entities: action.payload,
+          status: 'idle',
+        };
+      default:
+        return state;
+    }
+  }
